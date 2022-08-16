2fc2927cd4a797c36f9aa8fd479b7a78d3f030db
hanxiao <1104107863@qq.com>
2022-08-16 17:34:21 +0800

    配置页面

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="background-color: rgba(235, 235, 235, 0.08)">
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  created(){
+    
   }
-}
+};
 </script>
 
 <style>
